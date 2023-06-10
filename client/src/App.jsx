a68b38ca7951773000eb0eb41a87811867afc4fc
Shashank <you@example.com>
2023-06-10 20:28:12 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <>
+      <div className=" font-bold underline text-green-400">
+        This is App
+      </div>
+    </>
+  )
+}
+
+export default App
