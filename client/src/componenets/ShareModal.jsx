d32418d3a0f0d650926913dc7abf60db0f2ef348
Shashank <you@example.com>
2023-06-16 00:03:02 +0530

    photo upload modal completed

--- client/src/componenets/ShareModal.jsx
+++ client/src/componenets/ShareModal.jsx
@@ -1,28 +1,66 @@
 import { useState, useRef } from "react";
 import { Modal, Button, Group } from "@mantine/core";
+import { AiOutlineCloudUpload } from 'react-icons/ai'
+
 
 export default function ShareModal({ open, setOpen }) {
-    const [image, setImage] = useState("");
+    const initial = {
+        image: "",
+        caption: "",
+        date: new Date().getTime()
+    }
+    const [image, setImage] = useState(initial);
     const fileRef = useRef();
+
     const onImageChange = (e) => {
         const img = e.target.files?.[0];
-        if (img) setImage({ image: URL.createObjectURL(img) });
+        if (img) setImage({ ...image, image: URL.createObjectURL(img) });
+    }
+    const handleClose = () => {
+        setOpen(false);
+        setImage(initial);
+        console.log(image);
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (image.image) {
+            handleClose();
+        }
     }
     return (
         <>
             <Modal
                 opened={open}
-                onClose={() => setOpen(false)}
+                onClose={handleClose}
                 title="Share Post"
                 size="lg"
                 radius="md"
             >
-                <div className="p-4">
-                    <div className="h-[400px] bg-gray-500" onClick={() => fileRef.current.click()} > upload image
-                        <img src={image.image} />
+                <form className="p-4 flex flex-col gap-4 text-sm text-text-primary" onSubmit={handleSubmit}>
+                    <div className=" flex flex-col  items-center justify-center bg-background-secondary  border  min-h-[300px]" onClick={() => fileRef.current.click()} >
+                        {!image.image && <div className=" flex gap-4 items-center justify-center font-semibold h-full">
+                            <AiOutlineCloudUpload size={"20px"} />
+                            <h1>Please upload an image</h1>
+                        </div>}
+                        <img className=" max-h-[300px] object-contain " src={image?.image} />
                     </div>
-                </div>
-                <input className='hidden' type='file' ref={fileRef} onChange={onImageChange} />
+                    {/* profile  */}
+                    <div className=' flex gap-2 w-[90%] text-xs  items-center'>
+                        <img className='w-10 h-10 object-contain rounded-full bg-black' src="" alt='dp' />
+                        <div>
+                            <div className='font-semibold '>Shashank Jagtap</div>
+                            <span className=' dark:text-text-secondary-dark' >@shashak.jagtap</span>
+                        </div>
+                    </div>
+                    {/* caption  */}
+                    <div >
+                        <h1 className=" font-semibold">Caption </h1>
+                        <input className=" w-full  border-b-2 p-2 focus:outline-none" value={image?.caption} onChange={(e) => setImage({ ...image, caption: e.target.value })} />
+                    </div>
+
+                    <button type="submit" className='bg-accent rounded-xl p-1 py-2 text-white font-semibold'> Post</button>
+                </form>
+                <input className='hidden' type='file' accept="image/*" ref={fileRef} onChange={onImageChange} />
 
             </Modal>
 
